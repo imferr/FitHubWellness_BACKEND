2529f3e0798b791587b8a499b07c33cf96bafb80
imferr <117535778+imferr@users.noreply.github.com>
2023-11-13 21:59:10 -0400

    initial commit

---
+++ src/main/java/bo/edu/ucb/fithubwelness/FithubwelnessApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.fithubwelness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FithubwelnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FithubwelnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/fithubwelness/FithubwelnessApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.fithubwelness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FithubwelnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
