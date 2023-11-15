2aa4e5946a4ce61e1ff88e06d555a2a596625001
imferr <117535778+imferr@users.noreply.github.com>
2023-11-15 00:37:01 -0400

    creando user entity

---
+++ src/main/java/bo/edu/ucb/fithubwelness/entity/UserEntity.java
@@ -0,0 +1,80 @@
+package bo.edu.ucb.fithubwelness.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Integer userId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "birthday", nullable = false)
+    private Date birthday;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Integer userId, String name, String email, Date birthday) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    //setters:
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    //toString:
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
