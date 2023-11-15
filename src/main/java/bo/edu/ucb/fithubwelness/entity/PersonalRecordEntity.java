162077116f615eba692ccb756259dc087b5f76b7
imferr <117535778+imferr@users.noreply.github.com>
2023-11-15 01:02:43 -0400

    creando personal record entity

---
+++ src/main/java/bo/edu/ucb/fithubwelness/entity/PersonalRecordEntity.java
@@ -0,0 +1,94 @@
+package bo.edu.ucb.fithubwelness.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "personal_record")
+public class PersonalRecordEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "personalrecordid")
+    private Integer personalRecordId;
+
+    @Column(name = "weight", nullable = false)
+    private Double weight;
+
+    @Column(name = "repetitions", nullable = false)
+    private Integer repetitions;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @ManyToOne
+    @JoinColumn(name = "goal_goalid", referencedColumnName = "goalid", nullable = false)
+    private GoalEntity goalId;
+
+    public PersonalRecordEntity() {
+    }
+
+    public PersonalRecordEntity(Integer personalRecordId, Double weight, Integer repetitions, String date, GoalEntity goalId) {
+        this.personalRecordId = personalRecordId;
+        this.weight = weight;
+        this.repetitions = repetitions;
+        this.date = date;
+        this.goalId = goalId;
+    }
+
+    //getters:
+
+    public int getPersonalRecordId() {
+        return personalRecordId;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public Integer getRepetitions() {
+        return repetitions;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public GoalEntity getGoalId() {
+        return goalId;
+    }
+
+    //setters:
+
+    public void setPersonalRecordId(int personalRecordId) {
+        this.personalRecordId = personalRecordId;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public void setRepetitions(Integer repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setGoalId(GoalEntity goalId) {
+        this.goalId = goalId;
+    }
+
+    //toString:
+
+    @Override
+    public String toString() {
+        return "PersonalRecordDTO{" +
+                "personalRecordId=" + personalRecordId + 
+                ", weight=" + weight + 
+                ", repetitions=" + repetitions + 
+                ", date=" + date + 
+                ", goalId=" + goalId + 
+                '}';
+    }
+}
