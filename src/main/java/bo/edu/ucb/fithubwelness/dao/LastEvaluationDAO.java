32087e423587419d2336c4cef6a69ebc55c372fb
imferr <117535778+imferr@users.noreply.github.com>
2023-11-28 23:18:56 -0400

    creando last evaluation dao

---
+++ src/main/java/bo/edu/ucb/fithubwelness/dao/LastEvaluationDAO.java
@@ -0,0 +1,7 @@
+package bo.edu.ucb.fithubwelness.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LastEvaluationDAO extends JpaRepository<LastEvaluationDAO, Integer> {
+        
+}
