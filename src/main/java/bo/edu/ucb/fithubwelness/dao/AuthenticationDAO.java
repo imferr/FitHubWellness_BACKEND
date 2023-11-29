1e007a8788d5d940f3261aadc9fe9617304ee045
imferr <117535778+imferr@users.noreply.github.com>
2023-11-28 23:12:50 -0400

    creando last evaluation entity

--- src/main/java/bo/edu/ucb/fithubwelness/dao/AuthenticationDAO.java
+++
@@ -1,8 +0,0 @@
-package bo.edu.ucb.fithubwelness.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import bo.edu.ucb.fithubwelness.entity.AuthenticationEntity;
-
-public interface AuthenticationDAO extends JpaRepository<AuthenticationEntity, Integer> {
-    
-}
